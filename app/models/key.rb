8145a9355e1b7ea93b0f4c8ffd64246833bc9931
Ohad Levy <ohadlevy@gmail.com>
2011-08-24 13:23:55 +0300

    typo

--- app/models/key.rb
+++ app/models/key.rb
@@ -1,6 +1,6 @@
 class Key < ActiveRecord::Base
   belongs_to :resource
-  has_many :stack_keys, :deployments => :destroy
+  has_many :stack_keys, :dependent => :destroy
   has_many :stacks, :through => :stack_keys
   has_many :values
   validates_uniqueness_of :name, :scope => :resource_id
